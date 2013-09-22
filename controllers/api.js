6b45a541d440c5759f678f2b3137498363c0529a
Po-Ying Chen <poying.me@gmail.com>
2013-09-22 19:27:25 +0800

    更新 controller 寫法

--- controllers/api.js
+++ controllers/api.js
@@ -12,16 +12,16 @@ module.exports = function () {
   this.js = {
     name: 'jsfile',
     path: '/youmeb/youmeb-api-angular.js',
-    handler: function () {
-      this.response.sendfile(path.join(__dirname, '../youmeb.js'));
+    handler: function (req, res, next) {
+      res.sendfile(path.join(__dirname, '../youmeb.js'));
     }
   };
 
   this.minjs = {
     name: 'minjsfile',
     path: '/youmeb/youmeb-api-angular.min.js',
-    handler: function () {
-      this.response.sendfile(path.join(__dirname, '../youmeb.min.js'));
+    handler: function (req, res, next) {
+      res.sendfile(path.join(__dirname, '../youmeb.min.js'));
     }
   };
 
